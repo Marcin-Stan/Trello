5bc99e83866452b3b7c7797e50c69fa31bac29a7
Marcin-Stan <65038043+Marcin-Stan@users.noreply.github.com>
2021-03-22 19:14:31 +0100

    Initial commit

---
+++ src/main/java/com/restservice/RestApp/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.restservice.RestApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @RequestMapping("/1")
+    public String index(){
+        return "Karolak aktor wybitny";
+    }
+}
