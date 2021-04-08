3bf83a7aceb8f828d7f0bbe9b6a9ab7e09e04877
Michał Matusiak <60293254+s218156@users.noreply.github.com>
2021-04-08 22:10:55 +0200

    Update UserController.java

--- src/main/java/com/restservice/RestApp/controller/UserController.java
+++ src/main/java/com/restservice/RestApp/controller/UserController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 public class UserController {
-
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @Autowired
     UserRepository userRepository;
 
