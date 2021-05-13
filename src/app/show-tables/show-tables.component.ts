038032c847f987bb5ac2ab27ede305cb97c61552
s218156 <218156@edu.p.lodz.pl>
2021-05-13 21:03:01 +0200

    Dodano obsługę JVT Token i dodawanie tablicy

--- src/app/show-tables/show-tables.component.ts
+++ src/app/show-tables/show-tables.component.ts
@@ -1,8 +1,9 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {IUser} from "../user";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {IBoard} from "../board";
 import {IBoardUser} from "../board-user";
+import {IUserWithToken} from "../user-with-token";
 
 @Component({
   selector: 'app-show-tables',
@@ -11,19 +12,35 @@ import {IBoardUser} from "../board-user";
 })
 export class ShowTablesComponent implements OnInit {
   readonly ROOT_URL = 'https://pl-paw-2021.herokuapp.com/boardsUser/getByUser';
+  readonly ADD_BOARD_URL = 'https://pl-paw-2021.herokuapp.com/boards/add';
+  postData={};
   constructor(private http:HttpClient){
   }
   result:string;
   listOfBoards:IBoardUser[];
-  @Input() user :IUser;
+  @Input() userWithToken :IUserWithToken;
   ngOnInit(): void {
-    this.http.post<IBoardUser[]>(this.ROOT_URL,this.user).toPromise().then(data => {
 
+    const headers = new HttpHeaders()
+      .set("authorization",this.userWithToken.token);
+    this.http.post<IBoardUser[]>(this.ROOT_URL,this.userWithToken.user,{headers:headers}).toPromise().then(data => {
       console.log(data);
       this.listOfBoards=data;
-
-
     });
-
   }
-}
+  addBoard(boardName:string){
+    const headers = new HttpHeaders()
+      .set("authorization",this.userWithToken.token);
+    this.postData={
+      name:boardName,
+      owner:{
+        id:this.userWithToken.user.id,
+      }};
+
+    this.http.post(this.ADD_BOARD_URL,this.postData,{headers:headers}).subscribe(
+        res => {
+        });
+
+    }
+  }
+
