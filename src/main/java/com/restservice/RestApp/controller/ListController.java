3fe0d4458aacbbba7189a3c065afa2d9bda0a860
Marcin-Stan <65038043+Marcin-Stan@users.noreply.github.com>
2021-05-14 19:21:11 +0200

    Controller pobierający UseraByEmail

--- src/main/java/com/restservice/RestApp/controller/ListController.java
+++ src/main/java/com/restservice/RestApp/controller/ListController.java
@@ -40,9 +40,4 @@ public class ListController {
         return ResponseEntity.ok(listRepository.save(list));
     }
 
-
-
-
-
-
 }
--- src/main/java/com/restservice/RestApp/controller/UserController.java
+++ src/main/java/com/restservice/RestApp/controller/UserController.java
@@ -16,55 +16,10 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
 
-    /*
-    @PostMapping("/users/register")
-    public Status registerUser(@Valid @RequestBody User newUser) {
-        List<User> users = userRepository.findAll();
-        for (User user : users) {
-            if (user.equals(newUser)) {
-                return Status.USER_ALREADY_EXISTS;
-            }
-        }
-        userRepository.save(newUser);
-        return Status.SUCCESS;
-    }
 
-     */
-
-    @PostMapping("/users/login")
-    public Long loginUser(@Valid @RequestBody User user) {
-        List<User> users = userRepository.findAll();
-        User usertest = userRepository.findByEmail(user.getEmail());
-        String password = user.getPassword();
-        String email = user.getEmail();
-        for (User other : users) {
-            if (other.getPassword().equals(password) && other.getEmail().equals(email)) {
-                usertest.setEnabled(true);
-                userRepository.save(usertest);
-                return usertest.getId();
-            }
-        }
-        return -1L;
-    }
-
-
-
-    @PostMapping("/users/logout")
-    public Status logUserOut(@RequestBody User user) {
-        List<User> users = userRepository.findAll();
-        Optional<User> user1 = userRepository.findById(user.getId());
-
-        if(!user1.isEmpty()){
-            for (User other : users) {
-                if (other.equals(user1.get())) {
-                    user.setEnabled(false);
-                    userRepository.save(user1.get());
-                    return Status.SUCCESS;
-                }
-            }
-        }
-
-        return Status.FAILURE;
+    @PostMapping("/users/getUserByEmail")
+    public User loginUser(String email) {
+        return userRepository.findByEmail(email);
     }
 
     @DeleteMapping("/users/all")
