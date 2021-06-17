f3fdda8e539410962fba416c6d1f6aba61e78c40
s218156 <218156@edu.p.lodz.pl>
2021-06-17 21:09:03 +0200

    hot fix

--- src/app/comment.ts
+++ src/app/comment.ts
@@ -1,6 +1,9 @@
+import {IUser} from "./user";
+import {ICard} from "./card";
+
 export interface IComment {
   id: number;
-  card_id:number;
-  user_id:number;
+  card:ICard,
+  user:IUser,
   text:string;
 }
